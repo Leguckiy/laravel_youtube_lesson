fff8a1cff9ebb8e6d6624eb61f74517322ed4be4
Leguckiy <Leguckiy@gmail.com>
2023-04-22 14:05:06 +0300

    lesson_11

--- routes/web.php
+++ routes/web.php
@@ -11,19 +11,27 @@ Auth::routes([
 
 Route::get('/logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('get-logout');
 
-Route::group(['middlevare' => 'auth'], function() {
-    Route::get('/orders', 'App\Http\Controllers\Admin\OrderController@index')->name('home');
+Route::group([
+    'middleware' => 'auth',
+    'namespace' => 'App\Http\Controllers\Admin'
+], function() {
+    Route::group(['middleware' => 'is_admin'], function() {
+        Route::get('/orders', 'OrderController@index')->name('home');
+    });
 });
 
 Route::get('/', 'App\Http\Controllers\MainController@index')->name('index');
 Route::get('/categories', 'App\Http\Controllers\MainController@categories')->name('categories');
 
-
-Route::get('/basket', 'App\Http\Controllers\BasketController@basket')->name('basket');
-Route::get('/basket/place', 'App\Http\Controllers\BasketController@basketPlace')->name('basket-place');
-Route::post('/basket/add/{id}', 'App\Http\Controllers\BasketController@basketAdd')->name('basket-add');
-Route::post('/basket/remove/{id}', 'App\Http\Controllers\BasketController@basketRemove')->name('basket-remove');
-Route::post('/basket/confirm', 'App\Http\Controllers\BasketController@basketConfirm')->name('basket-confirm');
+Route::group(['prefix' => 'basket'], function() {
+    Route::post('/add/{id}', 'App\Http\Controllers\BasketController@basketAdd')->name('basket-add');
+    Route::group(['middleware' => 'basket_not_empty'], function() {
+        Route::get('/', 'App\Http\Controllers\BasketController@basket')->name('basket');
+        Route::get('/place', 'App\Http\Controllers\BasketController@basketPlace')->name('basket-place');
+        Route::post('/remove/{id}', 'App\Http\Controllers\BasketController@basketRemove')->name('basket-remove');
+        Route::post('/confirm', 'App\Http\Controllers\BasketController@basketConfirm')->name('basket-confirm');
+    });
+});
 
 Route::get('/{category}/{product}', 'App\Http\Controllers\MainController@product')->name('product');
 Route::get('/{category}', 'App\Http\Controllers\MainController@category')->name('category');
